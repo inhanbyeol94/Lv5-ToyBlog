8cc77ec21fe3f50a70a6fe9e7398d1b9b25e5c76
inhanbyeol <inhanbyeol94@gmail.com>
2023-07-09 23:54:16 +0900

    좋아요 구현방식 변경 및 rawQuery와 병합하여 카운트 반환

--- repositories/likes.repository.js
+++ repositories/likes.repository.js
@@ -1,4 +1,4 @@
-const { Like, Post, Member, sequelize } = require('../models');
+const { Like, Post, Member, Sequelize } = require('../models');
 
 class LikeRepository {
   switch = async (postId, id) => {
@@ -6,18 +6,22 @@ class LikeRepository {
 
     if (!switchLike) {
       await Like.create({ post_id: postId, user_id: id });
-      await Post.update({ like: (await Post.findOne({ where: { post_id: postId }, attributes: ['like'] })).like + 1 }, { where: { post_id: postId } });
+      return '좋아요가 등록되었습니다.';
     } else {
       await Like.destroy({ where: { post_id: postId, user_id: id } });
-      await Post.update({ like: (await Post.findOne({ where: { post_id: postId }, attributes: ['like'] })).like - 1 }, { where: { post_id: postId } });
+      return '좋아요가 취소되었습니다.';
     }
   };
   findAllLikePosts = async (id) => {
-    return await Like.findAll({
-      attributes: [],
-      where: { user_id: id },
-      include: [{ model: Post, attributes: { exclude: ['user_id'] }, include: [{ model: Member, attributes: { exclude: ['password', 'created_at', 'updated_at', 'user_id'] } }] }],
+    return await Post.findAll({
+      attributes: ['post_id', 'title', 'content', 'created_at', [Sequelize.literal(`(SELECT COUNT(*) FROM Likes WHERE Likes.post_id = Post.post_id)`), 'like']],
+      include: [
+        { model: Like, where: { user_id: id }, attributes: [] },
+        { model: Member, attributes: ['nickname'] },
+      ],
+      order: [['like', 'DESC']],
       raw: true,
+      nest: true,
     });
   };
 }
