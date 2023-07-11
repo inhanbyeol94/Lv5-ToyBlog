0df7ae8ba3b164cf8b1242515c5c3e6efc33dcdb
inhanbyeol <inhanbyeol94@gmail.com>
2023-07-11 22:17:03 +0900

    Posts 구조 변경

--- controllers/posts.controller.js
+++ controllers/posts.controller.js
@@ -4,28 +4,66 @@ class PostsController {
   postService = new PostService();
 
   getPosts = async (req, res, next) => {
-    const { code, result } = await this.postService.findAllPost();
-    return res.status(code).json({ posts: result });
+    try {
+      const { code, result } = await this.postService.findAllPost();
+      return res.status(code).json({ posts: result });
+    } catch (err) {
+      if (err.code) return res.status(err.code).json({ message: err.result });
+      console.error(err);
+      return res.status(500).json({ message: '오류가 발생하였습니다.' });
+    }
   };
 
   getPost = async (req, res, next) => {
-    const { code, result } = await this.postService.findOnePost(req);
-    return res.status(code).json({ post: result });
+    try {
+      const { postId } = req.params;
+      const { code, result } = await this.postService.findOnePost({ postId });
+      return res.status(code).json({ post: result });
+    } catch (err) {
+      if (err.code) return res.status(err.code).json({ message: err.result });
+      console.error(err);
+      return res.status(500).json({ message: '오류가 발생하였습니다.' });
+    }
   };
 
   createPost = async (req, res, next) => {
-    const { code, result, resultPostId } = await this.postService.createPost(req, res);
-    return res.status(code).json({ message: result, postId: resultPostId });
+    try {
+      const { title, content } = req.body;
+      const { id } = res.locals.user;
+      const { code, result, resultPostId } = await this.postService.createPost({ id, title, content });
+      return res.status(code).json({ message: result, postId: resultPostId });
+    } catch (err) {
+      if (err.code) return res.status(err.code).json({ message: err.result });
+      console.error(err);
+      return res.status(500).json({ message: '오류가 발생하였습니다.' });
+    }
   };
 
   updatePost = async (req, res, next) => {
-    const { code, result } = await this.postService.updatePost(req, res);
-    return res.status(code).json({ message: result });
+    try {
+      const { postId } = req.params;
+      const { title, content } = req.body;
+      const { id } = res.locals.user;
+      const { code, result } = await this.postService.updatePost({ postId, title, content, id });
+      return res.status(code).json({ message: result });
+    } catch (err) {
+      if (err.code) return res.status(err.code).json({ message: err.result });
+      console.error(err);
+      return res.status(500).json({ message: '오류가 발생하였습니다.' });
+    }
   };
 
   deletePost = async (req, res, next) => {
-    const { code, result } = await this.postService.deletePost(req, res);
-    return res.status(code).json({ message: result });
+    try {
+      const { postId } = req.params;
+      const { id } = res.locals.user;
+      const { code, result } = await this.postService.deletePost({ postId, id });
+      return res.status(code).json({ message: result });
+    } catch (err) {
+      if (err.code) return res.status(err.code).json({ message: err.result });
+      console.error(err);
+      return res.status(500).json({ message: '오류가 발생하였습니다.' });
+    }
   };
 }
 
--- services/posts.service.js
+++ services/posts.service.js
@@ -4,100 +4,64 @@ class PostService {
   postRepository = new PostRepository();
 
   findAllPost = async () => {
-    try {
-      const result = await this.postRepository.findAllPost();
-      return {
-        code: 201,
-        result: result.map((x) => {
-          return {
-            postId: x.post_id,
-            title: x.title,
-            nickname: x.Member.nickname,
-            content: x.content,
-            createdAt: new Date(x.created_at).toLocaleString(),
-            like: x.Likes.like,
-          };
-        }),
-      };
-    } catch (err) {
-      console.error(err);
-      return { code: 500, result: '오류가 발생하였습니다.' };
-    }
+    const result = await this.postRepository.findAllPost();
+    return {
+      code: 201,
+      result: result.map((x) => {
+        return {
+          postId: x.post_id,
+          title: x.title,
+          nickname: x.Member.nickname,
+          content: x.content,
+          createdAt: new Date(x.created_at).toLocaleString(),
+          like: x.Likes.like,
+        };
+      }),
+    };
   };
 
-  findOnePost = async (req) => {
-    try {
-      const { postId } = req.params;
-      const result = await this.postRepository.findOnePost({ post_id: postId });
-      if (!result) return { code: 404, result: '존재하지 않는 게시물입니다.' };
-      return {
-        code: 200,
-        result: {
-          postId: result.post_id,
-          title: result.title,
-          content: result.content,
-          nickname: result.Member.nickname,
-          like: result.like,
-          createdAt: new Date(result.created_at).toLocaleString(),
-        },
-      };
-    } catch (err) {
-      console.error(err);
-      return { code: 500, result: '오류가 발생하였습니다.' };
-    }
+  findOnePost = async ({ postId }) => {
+    const result = await this.postRepository.findOnePost({ post_id: postId });
+    if (!result) throw { code: 404, result: '존재하지 않는 게시물입니다.' };
+    return {
+      code: 200,
+      result: {
+        postId: result.post_id,
+        title: result.title,
+        content: result.content,
+        nickname: result.Member.nickname,
+        like: result.like,
+        createdAt: new Date(result.created_at).toLocaleString(),
+      },
+    };
   };
 
-  createPost = async (req, res) => {
-    try {
-      const { title, content } = req.body;
-      const { id } = res.locals.user;
-      const result = await this.postRepository.createPost({ user_id: id, title: title, content: content });
-      return { code: 201, result: '정상 등록되었습니다.', resultPostId: result.post_id };
-    } catch (err) {
-      console.error(err);
-      return { code: 500, result: '오류가 발생하였습니다.', resultPostId: null };
-    }
+  createPost = async ({ id, title, content }) => {
+    const result = await this.postRepository.createPost({ user_id: id, title: title, content: content });
+    return { code: 201, result: '정상 등록되었습니다.', resultPostId: result.post_id };
   };
 
-  updatePost = async (req, res) => {
-    try {
-      const { postId } = req.params;
-      const { title, content } = req.body;
-      const { id } = res.locals.user;
+  updatePost = async ({ postId, title, content, id }) => {
+    const findPost = await this.postRepository.findOnePost({ post_id: postId });
+    if (!findPost) throw { code: 404, result: '존재하지 않는 게시물입니다.' };
 
-      const findPost = await this.postRepository.findOnePost({ post_id: postId });
-      if (!findPost) return { code: 404, result: '존재하지 않는 게시물입니다.' };
+    const postAuthorValid = await this.postRepository.findOnePost({ post_id: postId, user_id: id });
+    if (!postAuthorValid) throw { code: 401, result: '본인이 작성한 게시물만 수정이 가능합니다.' };
 
-      const postAuthorValid = await this.postRepository.findOnePost({ post_id: postId, user_id: id });
-      if (!postAuthorValid) return { code: 401, result: '본인이 작성한 게시물만 수정이 가능합니다.' };
-
-      await this.postRepository.updatePost({ title: title, content: content }, [{ post_id: postId }, { user_id: id }]);
-
-      return { code: 200, result: '게시물이 정상 수정되었습니다.' };
-    } catch (err) {
-      console.error(err);
-      return { code: 500, result: '오류가 발생하였습니다.' };
-    }
+    await this.postRepository.updatePost({ title: title, content: content }, [{ post_id: postId }, { user_id: id }]);
+    return { code: 200, result: '게시물이 정상 수정되었습니다.' };
   };
 
-  deletePost = async (req, res) => {
-    try {
-      const { postId } = req.params;
-      const { id } = res.locals.user;
+  deletePost = async ({ postId, id }) => {
+    const findPost = await this.postRepository.findOnePost({ post_id: postId });
+    if (!findPost) throw { code: 404, result: '존재하지 않는 게시물입니다.' };
 
-      const findPost = await this.postRepository.findOnePost({ post_id: postId });
-      if (!findPost) return { code: 404, result: '존재하지 않는 게시물입니다.' };
+    const postAuthorValid = await this.postRepository.findOnePost({ post_id: postId, user_id: id });
+    if (!postAuthorValid) throw { code: 401, result: '본인이 작성한 게시물만 삭제가 가능합니다.' };
 
-      const postAuthorValid = await this.postRepository.findOnePost({ post_id: postId, user_id: id });
-      if (!postAuthorValid) return { code: 401, result: '본인이 작성한 게시물만 삭제가 가능합니다.' };
+    await this.postRepository.deletePost([{ post_id: postId }, { user_id: id }]);
 
-      await this.postRepository.deletePost([{ post_id: postId }, { user_id: id }]);
-
-      return { code: 200, result: '게시물이 정상 삭제되었습니다.' };
-    } catch (err) {
-      console.error(err);
-      return { code: 500, result: '오류가 발생하였습니다.' };
-    }
+    return { code: 200, result: '게시물이 정상 삭제되었습니다.' };
   };
 }
 
