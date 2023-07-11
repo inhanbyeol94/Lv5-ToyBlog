b5a04c6270dcea7fdcb6f5306b404905c7be2db5
inhanbyeol <inhanbyeol94@gmail.com>
2023-07-11 22:03:43 +0900

    Members 구조 변경

--- controllers/members.controller.js
+++ controllers/members.controller.js
@@ -4,13 +4,28 @@ class MembersController {
   memberService = new MemberService();
 
   signUp = async (req, res, next) => {
-    const { code, result } = await this.memberService.createMember(req);
-    return res.status(code).json({ message: result });
+    try {
+      const { id, nickname, password } = req.body;
+      const { code, result } = await this.memberService.createMember({ id, nickname, password });
+      return res.status(code).json({ message: result });
+    } catch (err) {
+      if (err.code) return res.status(err.code).json({ message: err.result });
+      console.error(err);
+      return res.status(500).json({ message: '오류가 발생하였습니다.' });
+    }
   };
 
   login = async (req, res, next) => {
-    const { code, result } = await this.memberService.login(req, res);
-    return res.status(code).json({ message: result });
+    try {
+      const { id, password } = req.body;
+      const { code, result, token } = await this.memberService.login({ id, password });
+      res.cookie('auth', `Bearer ${token}`);
+      return res.status(code).json({ message: result });
+    } catch (err) {
+      if (err.code) return res.status(err.code).json({ message: err.result });
+      console.error(err);
+      return res.status(500).json({ message: '오류가 발생하였습니다.' });
+    }
   };
 }
 
