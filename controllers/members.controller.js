726d2c7958beb2317a4ffe168ac08278aff4f074
inhanbyeol <inhanbyeol94@gmail.com>
2023-07-12 20:46:52 +0900

    Members repository 구조변경에 의한 추가작업

--- controllers/members.controller.js
+++ controllers/members.controller.js
@@ -1,32 +1,33 @@
 const MemberService = require('../services/members.service');
 
 class MembersController {
-  memberService = new MemberService();
+    memberService = new MemberService();
 
-  signUp = async (req, res) => {
-    try {
-      const { id, nickname, password } = req.body;
-      const { code, result } = await this.memberService.createMember({ id, nickname, password });
-      return res.status(code).json({ message: result });
-    } catch (err) {
-      if (err.code) return res.status(err.code).json({ message: err.result });
-      console.error(err);
-      return res.status(500).json({ message: '오류가 발생하였습니다.' });
-    }
-  };
+    signUp = async (req, res) => {
+        try {
+            const { id, nickname, password } = req.body;
+            const { code, result } = await this.memberService.createMember({ id, nickname, password });
+            return res.status(code).json({ message: result });
+        } catch (err) {
+            if (err.code) return res.status(err.code).json({ message: err.result });
+            console.error(err);
+            return res.status(500).json({ message: '오류가 발생하였습니다.' });
+        }
+    };
 
-  login = async (req, res) => {
-    try {
-      const { id, password } = req.body;
-      const { code, result, token } = await this.memberService.login({ id, password });
-      res.cookie('auth', `Bearer ${token}`);
-      return res.status(code).json({ message: result });
-    } catch (err) {
-      if (err.code) return res.status(err.code).json({ message: err.result });
-      console.error(err);
-      return res.status(500).json({ message: '오류가 발생하였습니다.' });
-    }
-  };
+    login = async (req, res) => {
+        try {
+            const { id, password } = req.body;
+            const { code, result, token } = await this.memberService.login({ id, password });
+
+            res.cookie('auth', `Bearer ${token}`);
+            return res.status(code).json({ message: result });
+        } catch (err) {
+            if (err.code) return res.status(err.code).json({ message: err.result });
+            console.error(err);
+            return res.status(500).json({ message: '오류가 발생하였습니다.' });
+        }
+    };
 }
 
 module.exports = MembersController;
--- services/members.service.js
+++ services/members.service.js
@@ -9,10 +9,10 @@ class MemberService {
     memberRepository = new MemberRepository();
 
     createMember = async ({ id, nickname, password }) => {
-        const overlapId = await this.memberRepository.findOne({ user_id: id });
+        const overlapId = await this.memberRepository.findOne([{ user_id: id }]);
         if (overlapId) throw { code: 405, result: '이미 사용중인 아이디입니다.' };
 
-        const overlapNickname = await this.memberRepository.findOne({ nickname: nickname });
+        const overlapNickname = await this.memberRepository.findOne([{ nickname: nickname }]);
         if (overlapNickname) throw { code: 405, result: '이미 사용중인 닉네임입니다.' };
 
         const passwordToCrypto = crypto.pbkdf2Sync(password, SECRET_KEY.toString('hex'), 11524, 64, 'sha512').toString('hex');
@@ -23,7 +23,7 @@ class MemberService {
 
     login = async ({ id, password }) => {
         const passwordToCrypto = crypto.pbkdf2Sync(password, SECRET_KEY.toString('hex'), 11524, 64, 'sha512').toString('hex');
-        const findUser = await this.memberRepository.findOne({ user_id: id, password: passwordToCrypto });
+        const findUser = await this.memberRepository.findOne([{ user_id: id }, { password: passwordToCrypto }]);
 
         if (!findUser) throw { code: 401, result: '아이디와 패스워드가 일치하지 않습니다.' };
         const token = await jwt.sign({ id: findUser.id, user_id: findUser.user_id, nickname: findUser.nickname }, SESSION_SECRET_KEY);
