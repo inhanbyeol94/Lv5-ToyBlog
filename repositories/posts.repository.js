b1c81a4d8a8b3b0d6b35be36ba8320a6dcfb56f3
inhanbyeol <inhanbyeol94@gmail.com>
2023-07-12 20:38:17 +0900

    repository 재사용성을 위해 where 의 경우 Op.and 전체적용

--- repositories/posts.repository.js
+++ repositories/posts.repository.js
@@ -2,32 +2,32 @@ const { Post, Member, Like } = require('../models');
 const { Op, Sequelize } = require('sequelize');
 
 class PostRepository {
-  findAllPost = async () => {
-    return await Post.findAll({
-      include: [
-        { model: Member, attributes: ['nickname'] },
-        { model: Like, attributes: [[Sequelize.literal(`(SELECT COUNT(*) FROM Likes WHERE post_id = Post.post_id)`), 'like']] },
-      ],
-      raw: true,
-      nest: true,
-    });
-  };
+    findAllPost = async () => {
+        return await Post.findAll({
+            include: [
+                { model: Member, attributes: ['nickname'] },
+                { model: Like, attributes: [[Sequelize.literal(`(SELECT COUNT(*) FROM Likes WHERE post_id = Post.post_id)`), 'like']] },
+            ],
+            raw: true,
+            nest: true,
+        });
+    };
 
-  findOnePost = async (target) => {
-    return await Post.findOne({ where: target, include: [{ model: Member }], attributes: { include: [[Sequelize.literal(`(SELECT COUNT(*) FROM Likes WHERE post_id = Post.post_id)`), 'like']] }, raw: true, nest: true });
-  };
+    findOnePost = async (target) => {
+        return await Post.findOne({ where: { [Op.and]: target }, include: [{ model: Member }], attributes: { include: [[Sequelize.literal(`(SELECT COUNT(*) FROM Likes WHERE post_id = Post.post_id)`), 'like']] }, raw: true, nest: true });
+    };
 
-  createPost = async (data) => {
-    return await Post.create(data);
-  };
+    createPost = async (data) => {
+        return await Post.create(data);
+    };
 
-  updatePost = async (data, target) => {
-    return await Post.update(data, { where: { [Op.and]: target } });
-  };
+    updatePost = async (data, target) => {
+        return await Post.update(data, { where: { [Op.and]: target } });
+    };
 
-  deletePost = async (target) => {
-    return await Post.destroy({ where: { [Op.and]: target } });
-  };
+    deletePost = async (target) => {
+        return await Post.destroy({ where: { [Op.and]: target } });
+    };
 }
 
 module.exports = PostRepository;
--- services/posts.service.js
+++ services/posts.service.js
@@ -1,65 +1,65 @@
 const PostRepository = require('../repositories/posts.repository');
 
 class PostService {
-  postRepository = new PostRepository();
+    postRepository = new PostRepository();
 
-  findAllPost = async () => {
-    const result = await this.postRepository.findAllPost();
-    return {
-      code: 201,
-      result: result.map((x) => {
+    findAllPost = async () => {
+        const result = await this.postRepository.findAllPost();
         return {
-          postId: x.post_id,
-          title: x.title,
-          nickname: x.Member.nickname,
-          content: x.content,
-          createdAt: new Date(x.created_at).toLocaleString(),
-          like: x.Likes.like,
+            code: 201,
+            result: result.map((x) => {
+                return {
+                    postId: x.post_id,
+                    title: x.title,
+                    nickname: x.Member.nickname,
+                    content: x.content,
+                    createdAt: new Date(x.created_at).toLocaleString(),
+                    like: x.Likes.like,
+                };
+            }),
         };
-      }),
     };
-  };
 
-  findOnePost = async ({ postId }) => {
-    const result = await this.postRepository.findOnePost({ post_id: postId });
-    if (!result) throw { code: 404, result: '존재하지 않는 게시물입니다.' };
-    return {
-      code: 200,
-      result: {
-        postId: result.post_id,
-        title: result.title,
-        content: result.content,
-        nickname: result.Member.nickname,
-        like: result.like,
-        createdAt: new Date(result.created_at).toLocaleString(),
-      },
+    findOnePost = async ({ postId }) => {
+        const result = await this.postRepository.findOnePost([{ post_id: postId }]);
+        if (!result) throw { code: 404, result: '존재하지 않는 게시물입니다.' };
+        return {
+            code: 200,
+            result: {
+                postId: result.post_id,
+                title: result.title,
+                content: result.content,
+                nickname: result.Member.nickname,
+                like: result.like,
+                createdAt: new Date(result.created_at).toLocaleString(),
+            },
+        };
     };
-  };
 
-  createPost = async ({ id, title, content }) => {
-    const result = await this.postRepository.createPost({ user_id: id, title: title, content: content });
-    return { code: 201, result: '정상 등록되었습니다.', resultPostId: result.post_id };
-  };
+    createPost = async ({ id, title, content }) => {
+        const result = await this.postRepository.createPost({ user_id: id, title: title, content: content });
+        return { code: 201, result: '정상 등록되었습니다.', resultPostId: result.post_id };
+    };
 
-  updatePost = async ({ postId, title, content, id }) => {
-    const findPost = await this.postRepository.findOnePost({ post_id: postId });
+    updatePost = async ({ postId, title, content, id }) => {
+        const findPost = await this.postRepository.findOnePost([{ post_id: postId }]);
 
-    if (!findPost) throw { code: 404, result: '존재하지 않는 게시물입니다.' };
-    if (findPost.user_id !== id) throw { code: 401, result: '본인이 작성한 게시물만 수정이 가능합니다.' };
+        if (!findPost) throw { code: 404, result: '존재하지 않는 게시물입니다.' };
+        if (findPost.user_id !== id) throw { code: 401, result: '본인이 작성한 게시물만 수정이 가능합니다.' };
 
-    await this.postRepository.updatePost({ title: title, content: content }, [{ post_id: postId }, { user_id: id }]);
-    return { code: 200, result: '게시물이 정상 수정되었습니다.' };
-  };
+        await this.postRepository.updatePost({ title: title, content: content }, [{ post_id: postId }, { user_id: id }]);
+        return { code: 200, result: '게시물이 정상 수정되었습니다.' };
+    };
 
-  deletePost = async ({ postId, id }) => {
-    const findPost = await this.postRepository.findOnePost({ post_id: postId });
-    if (!findPost) throw { code: 404, result: '존재하지 않는 게시물입니다.' };
-    if (findPost.user_id !== id) throw { code: 401, result: '본인이 작성한 게시물만 삭제가 가능합니다.' };
+    deletePost = async ({ postId, id }) => {
+        const findPost = await this.postRepository.findOnePost([{ post_id: postId }]);
+        if (!findPost) throw { code: 404, result: '존재하지 않는 게시물입니다.' };
+        if (findPost.user_id !== id) throw { code: 401, result: '본인이 작성한 게시물만 삭제가 가능합니다.' };
 
-    await this.postRepository.deletePost([{ post_id: postId }, { user_id: id }]);
+        await this.postRepository.deletePost([{ post_id: postId }, { user_id: id }]);
 
-    return { code: 200, result: '게시물이 정상 삭제되었습니다.' };
-  };
+        return { code: 200, result: '게시물이 정상 삭제되었습니다.' };
+    };
 }
 
 module.exports = PostService;
