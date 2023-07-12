d00ac1d6bc91ff6fd8a320f06973963efad95ba8
inhanbyeol <inhanbyeol94@gmail.com>
2023-07-12 20:52:09 +0900

    findAll, raw query를 통해 like 카운트 작업진행하며, Like모델 불필요

--- repositories/posts.repository.js
+++ repositories/posts.repository.js
@@ -1,13 +1,11 @@
-const { Post, Member, Like } = require('../models');
+const { Post, Member } = require('../models');
 const { Op, Sequelize } = require('sequelize');
 
 class PostRepository {
     findAll = async () => {
         return await Post.findAll({
-            include: [
-                { model: Member, attributes: ['nickname'] },
-                { model: Like, attributes: [[Sequelize.literal(`(SELECT COUNT(*) FROM Likes WHERE post_id = Post.post_id)`), 'like']] },
-            ],
+            include: [{ model: Member }],
+            attributes: { include: [[Sequelize.literal(`(SELECT COUNT(*) FROM Likes WHERE post_id = Post.post_id)`), 'like']] },
             raw: true,
             nest: true,
         });
